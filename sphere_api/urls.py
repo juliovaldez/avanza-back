5a3104a0df3f3aa9b2884a38ccf718544daf8654
Chaday Omar <chaday.1497@gmail.com>
2025-10-17 16:31:17 -0600

    Iniciando

--- sphere_api/urls.py
+++ sphere_api/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from api import api_urls
-    
+from django.shortcuts import redirect
+
 urlpatterns = [
+    path('', lambda request: redirect('/admin/')),
     path('admin/', admin.site.urls),
     path('api/',include(api_urls))
 ]
